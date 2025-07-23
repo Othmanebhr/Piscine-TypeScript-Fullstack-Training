8d591ede0218ef6d34b578b7b7e48d6dad5b9782
Othmane Bouhoura <obouhoura@gmail.com>
2025-07-23 20:24:24 +0200

    Day00 OK version TS + JS

---
+++ Day00/ex02/type_inference.ts
@@ -0,0 +1,37 @@
+/*Version TypeScript*/
+// function greet(name: string)
+// {
+// 	return `Hello, ${name}!`;
+// }
+
+// function multiply(x: number, y: number)
+// {
+// 	return x * y;
+// }
+
+// function isEven(n: number)
+// {
+// 	return n % 2 == 0;
+// }
+
+function greet(name)
+{
+	return `Hello, ${name}!`; //Backsticks 
+	// return "Hello" + name + "!";
+}
+
+function multiply(x, y)
+{
+	return x * y;
+}
+
+function isEven(n)
+{
+	return n % 2 == 0;
+}
+
+
+console.log(greet("Othmane"));
+console.log(multiply(2, 2));
+console.log(isEven(5));
+console.log(isEven(4));
