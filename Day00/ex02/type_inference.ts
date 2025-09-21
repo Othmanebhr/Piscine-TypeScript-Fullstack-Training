2263c819fe2fa72b1195fb5b57631abbf1be3128
root <obouhoura@gmail.com>
2025-09-21 13:45:45 +0200

    Day00 revu, ajout de details + ajout du fichier config et Build/

--- Day00/ex02/type_inference.ts
+++ Day00/ex02/type_inference.ts
@@ -1,37 +1,47 @@
 /*Version TypeScript*/
-// function greet(name: string)
-// {
-// 	return `Hello, ${name}!`;
-// }
-
-// function multiply(x: number, y: number)
-// {
-// 	return x * y;
-// }
-
-// function isEven(n: number)
-// {
-// 	return n % 2 == 0;
-// }
-
-function greet(name)
+function greet(name: string) //Sans inférence
 {
-	return `Hello, ${name}!`; //Backsticks 
-	// return "Hello" + name + "!";
+	return `Hello, ${name}!`;
 }
 
-function multiply(x, y)
+function greet2(name: string) : string // Avec inférence
+{
+	return `Hello, ${name}!`;
+}
+
+function multiply(x: number, y: number)
 {
 	return x * y;
 }
 
-function isEven(n)
+function isEven(n: number)
 {
 	return n % 2 == 0;
 }
+
+/*Version TypeScript*/
+// function greet(name)
+// {
+// 	return `Hello, ${name}!`; //Backsticks 
+// 	// return "Hello" + name + "!";
+// }
+
+// function multiply(x, y)
+// {
+// 	return x * y;
+// }
+
+// function isEven(n)
+// {
+// 	return n % 2 == 0;
+// }
 
 
 console.log(greet("Othmane"));
 console.log(multiply(2, 2));
 console.log(isEven(5));
 console.log(isEven(4));
+
+/*
+🧠 L'Inférence de Type: TypeScript devine automatiquement le type d'une variable/fonction sans que le précisier.
+*/
