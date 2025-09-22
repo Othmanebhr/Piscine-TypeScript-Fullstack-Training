a187104a370dc35f260bf5c2ee5ee0989db7ec6f
TeTop <obouhoura@gmail.com>
2025-09-22 23:19:14 +0200

    Day04 complete

---
+++ Day04/ex01/main.ts
@@ -0,0 +1,4 @@
+import {Geometry} from "./namespaces";
+
+console.log(Geometry.calculateArea(5,5), Geometry.calculatePerimeter(5,5))
+// console.log(Geometry.testOutsideWithoutExport()) // Don't work!
---
+++ Day04/ex03/src/index.ts
@@ -0,0 +1,4 @@
+function greet(name: string)
+{ console.log(`Hello! I am ${name}, and i'm doing my own Typescript Piscine!`); }
+
+greet("Te");
