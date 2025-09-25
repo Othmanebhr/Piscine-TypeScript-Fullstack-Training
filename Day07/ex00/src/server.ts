413bf5a9a8e64ffaa203bd3e483a786be0834151
TeTop <obouhoura@gmail.com>
2025-09-25 14:51:50 +0200

    Splitting days...

--- Day07/ex00/src/server.ts
+++ Day07/ex00/src/server.ts
@@ -1,4 +1,31 @@
-import express from 'express';
+import express from 'express'
 
 const app = express();
-console.log('Express is installed and imported successfully!')
+
+app.get('', (req,res) => {
+    console.log("Someone conected");
+    res.send("This is the front page!");
+})
+
+app.get('/hello', (req,res) =>{
+    res.send("Hello API!");
+})
+
+app.listen(3000, () => console.log("http://localhost:3000, http://localhost:3000/hello"));
+
+
+/*
+
+- On import express
+
+- On creer notre app avec express() => qui nous creer automatiquement notre serveur
+
+- req,res
+    - req: donnée reçu par notre serveur
+    - res: "response", réponse qu'on renvoie
+
+- res.send envoie une reponse directement;
+
+- app.listen, on ecoute sur le port 3000.
+
+*/
---
+++ ancien/DELETED/ex01/server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the API');
+});
+
+app.get('/hello', (req, res) => {
+    res.send('Hello API');
+}
+)
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`)
+});
