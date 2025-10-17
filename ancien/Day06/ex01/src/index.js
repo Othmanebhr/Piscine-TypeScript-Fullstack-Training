a52504292a7ecf720486d53f829f1db90661430d
TeTop <obouhoura@gmail.com>
2025-10-17 15:14:25 +0200

    Adding Old branch

--- ancien/Day06/ex01/src/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-console.log("Ceci est un test d'exec");
-//# sourceMappingURL=index.js.map
--- ancien/Day07/ex00/src/server.ts
+++
@@ -1,4 +0,0 @@
-import express from 'express';
-
-const app = express();
-console.log('Express is installed and imported successfully!')
